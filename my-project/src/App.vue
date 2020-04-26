ee08fec385e4753c4b5d6f168a7790ace1ec50e6
yuki <y.uehara@forbis.jp>
2020-04-26 18:30:22 +0900

    add sample router

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -2,31 +2,21 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/page1">page1</router-link>
+      <router-link to="/page2">page2</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss">
+#nav{
+  a{
+    color: #42b983;
+    text-decoration: none;
+  }
+  .router-link-exact-active{
+    color: #00252b;
+  }
 }
 </style>
---
+++ my-project/src/router/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Page1 from './views/Page1.vue'
+import Page2 from './views/Page2.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/page1',
+      name: 'page1',
+      component: Page1
+    },
+    {
+      path: '/page2',
+      name: 'page2',
+      component: Page2
+    }
+  ]
+})
